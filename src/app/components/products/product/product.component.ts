48e890630ea5ca82951e65835c95b175faf4a66c
mitsuoysharag <mitsuoysharag@gmail.com>
2018-05-23 00:43:03 -0500

    first commit

---
+++ src/app/components/products/product/product.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../../services/product.service';
+import { NgForm } from '@angular/forms';
+import { Product } from '../../../models/product';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor(
+    public productService: ProductService,
+    private toastr:ToastrService
+  ) { }
+
+  ngOnInit() {
+    this.productService.getProducts();
+    this.resetForm();
+  }
+
+  onSubmit(productForm:NgForm){
+    if(productForm.value.$key == null){
+      this.productService.insertProduct(productForm.value);
+    }else{
+      this.productService.updateProduct(productForm.value);
+    }
+    this.resetForm(productForm);
+    this.toastr.success('Successfull Operation','Operación Exitosa');
+  }
+
+  resetForm(productForm?:NgForm){
+    if(productForm!=null){
+      productForm.reset();
+      this.productService.selectedProduct = new Product();
+    }
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDm3o4d3-DsLfTbfZid6PQ_JyKENsuDWJo",
+    authDomain: "angular-firebase-mitsuoysharag.firebaseapp.com",
+    databaseURL: "https://angular-firebase-mitsuoysharag.firebaseio.com",
+    projectId: "angular-firebase-mitsuoysharag",
+    storageBucket: "",
+    messagingSenderId: "28777657673"
+  }
+};
